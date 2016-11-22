e20c7063bb7b92db468c6b334dd818ad5e8be349
Luka Vandervelden <lukc@upyum.com>
2016-11-22 11:00:30 +0100

    hash-file.c: primitive command-line parsing

--- hash-file.c
+++ hash-file.c
@@ -9,37 +9,94 @@
 
 #include "hash.h"
 
+struct options {
+	char colors;
+};
+
 void
 usage(const char* progname)
 {
 	fprintf(stderr, "usage: %s filename\n", progname);
 }
 
-int
-main(int argc, const char** argv)
+void
+file_hashes(const char* filename, struct options* opt)
 {
 	HashData* hd;
+	hd = hash_data_new(filename);
 
-	if (argc != 2) {
-		usage(argv[0]);
-		exit(1);
-	}
-
-	hd = hash_data_new(argv[1]);
+	if (opt->colors)
+		printf("\033[01;37m");
 
 	printf("File:      ");
+
+	if (opt->colors)
+		printf("\033[01;34m");
+
 	print_hash(hd->digest);
+
+	if (opt->colors)
+		printf("\033[00m");
+
 	printf("\n");
 
 	for (int i = 0; i < hd->chunkDigests.length; i++) {
-		int j = printf("Chunk %i: ", i);
+		int j;
+
+		if (opt->colors)
+			printf("\033[01;32m");
+
+		j = printf("Chunk %i: ", i);
 		for (; j < 11; j++)
 			printf(" ");
 
+		if (opt->colors)
+			printf("\033[01;34m");
+
 		print_hash(hd->chunkDigests.data[i]);
 		printf("\n");
+
+		if (opt->colors)
+			printf("\033[00m");
 	}
 
 	hash_data_free(hd);
+}
+
+int
+main(int argc, const char** argv)
+{
+	int i;
+	struct options opt;
+
+	memset(&opt, 0, sizeof(opt));
+
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			if (argv[i][1] == '-') {
+				if (!strcmp(argv[i], "--color")) {
+					opt.colors = 1;
+				} else {
+					usage(argv[0]);
+					exit(1);
+				}
+			} else {
+				int j;
+
+				for (j = 1; argv[i][j]; j++) {
+					switch (argv[i][j]) {
+						case 'c':
+							opt.colors = 1;
+							break;
+						default:
+							usage(argv[0]);
+							exit(1);
+					}
+				}
+			}
+		} else {
+			file_hashes(argv[i], &opt);
+		}
+	}
 }
 
