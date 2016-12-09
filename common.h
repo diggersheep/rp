9a3b5f3e2ccec8e7155dde546d2230faadfa175c
Luka Vandervelden <lukc@upyum.com>
2016-12-09 14:29:45 +0100

    Packet structures.

--- common.h
+++ common.h
@@ -2,6 +2,8 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdint.h>
+
 #define CHUNK_SIZE 1000 * 1000
 
 #include "vec/vec.h"
@@ -11,6 +13,34 @@ typedef struct {
 	unsigned char* digest;
 	vec_void_t chunkDigests;
 } HashData;
+
+typedef enum {
+	REQUEST_GET,
+	REQUEST_GET_ANSWER,
+	REQUEST_LIST,
+	REQUEST_LIST_ANSWER,
+
+	/* Debug/orz */
+	REQUEST_PRINT
+} RequestType;
+
+typedef struct {
+	unsigned char type;
+	unsigned char file_hash[32];
+	unsigned char chunk_hash[32];
+} RequestGet;
+
+typedef struct {
+	unsigned char type;
+	unsigned file_hash[32];
+} RequestList;
+
+typedef struct {
+	unsigned char type;
+	unsigned char file_hash[32];
+	uint16_t chunks_count;
+	unsigned char chunk_hashes[32][0];
+} RequestListAnswer;
 
 #endif
 
