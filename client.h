e1d581d74920bff5e11db91817c4dd12b2362239
diggerhseep <alexandre.combeau@outlook.fr>
2016-12-12 19:17:55 +0100

    g

--- client.h
+++ client.h
@@ -14,12 +14,13 @@ typedef struct {
 		STATUS_PUT,
 		STATUS_KEEP_ALIVE,
 		STATUS_GET,
+		STATUS_GET_CLIENT,
 		STATUS_LIST
 	} status;
 	int timeout;
 
-	vec_void_t related_clients; //SegmentClient
-	vec_void_t received_fragments; /* of int[1000] */
+	vec_void_t related_clients;    /* SegmentClient */
+	vec_void_t received_fragments; /* of int[1000]  */
 } RegisteredFile;
 
 #endif
